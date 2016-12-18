9572977779f08a88eb3339a8d5187881a34525b8
nsgolosoff <nsgolosov@edu.hse.ru>
2016-12-18 18:39:33 +0400

    add enities

---
+++ bookworm/bookworm/Entities/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace bookworm.Entities
+{
+    public class Book
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Autor { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public double Price { get; set; }
+        public double Count { get; set; }
+        public string ISBN { get; set; }
+    }
+}
---
+++ bookworm/bookworm/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace bookworm.Entities
+{
+    public class Order
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public Book Book { get; set; }
+        [Required]
+        public Client Client { get; set; }
+    }
+}
